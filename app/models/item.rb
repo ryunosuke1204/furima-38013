6e3ef14a91a3b87fe1dd9e3f6adb3aeb64894a61
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-26 17:50:33 +0900

    バリデーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,16 @@
 class Item < ApplicationRecord
-ActiveHash::Associations::ActiveRecordExtensions
-
-has_one :purchase_management
-belongs_to :user
-has_one_attached :image
+extend ActiveHash::Associations::ActiveRecordExtensions
 
 belongs_to :category
 belongs_to :item_condition
 belongs_to :postage_memu
 belongs_to :preparation_day
 belongs_to :sipping_area
+
+has_one :purchase_management
+belongs_to :user
+has_one_attached :image
+
 
 
 validates :item_name, presence: true
@@ -20,5 +21,11 @@ validates :postage_memu_id, presence: true
 validates :preparation_day_id, presence: true
 validates :sipping_area_id, presence: true
 validates :price, presence: true
+validates :title, :text, presence: true
+validates :sipping_area_id, numericality: { other_than: 1,message: "can't be blank" } 
+validates :preparation_day, numericality: { other_than: 1,message: "can't be blank" } 
+validates :item_condition_id, numericality: { other_than: 1,message: "can't be blank" } 
+validates :category_id, numericality: { other_than: 1,message: "can't be blank" } 
+validates :postage_memu_id, numericality: { other_than: 1,message: "can't be blank" } 
 
 end
