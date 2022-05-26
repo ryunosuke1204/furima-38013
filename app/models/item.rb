f85618bb485a198b228e56bcd94ae307865205be
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-26 11:17:58 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,13 @@ has_one :purchase_management
 belongs_to :user
 has_one_attached :image
 
-validates :column, presence: true
+validates :item_name, presence: true
+validates :introduction, presence: true
+validates :category_id, presence: true
+validates :item_condition_id, presence: true
+validates :postage_memu_id, presence: true
+validates :preparation_day_id, presence: true
+validates :sipping_area_id, presence: true
+validates :price, presence: true
 
 end
