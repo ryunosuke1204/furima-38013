499697124bc3e93e0edb19dd17a1e22a452a037e
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-27 20:44:12 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,6 +62,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price Half-width number')
       end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
