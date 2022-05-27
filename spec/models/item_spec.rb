1c4e9225fad491e578318e4cd5542057b850a64e
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-27 21:14:39 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,6 +67,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
       end
+      it '価格が300円未満では出品できない' do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it '価格が9_999_999円を超えると出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
     end
   end
 end
