fd4c278248d6247f4d69ebaeb607cd2da12dd0aa
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-28 17:36:27 +0900

    商品一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    # @items = Item.all
-    # @items = Item.order('created_at DESC')
+     @items = Item.all
+     @items = Item.all.order(created_at: :desc)
   end
 
   def new
@@ -24,6 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    
   end
 
   private
