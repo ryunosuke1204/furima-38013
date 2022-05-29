5a2d2dd51992831c7efb17eba74aac328e45c42b
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-05-29 10:30:31 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,18 +13,31 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
 
     if @item.save
-      redirect_to action: :index
+      redirect_to root_path
     else
       render :new
     end
   end
 
-  def edit
-  end
-
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+  @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+    redirect_to root_path 
+    end
+  end
+
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path
+  else
+    render :edit
+  end
+end
 
   private
 
