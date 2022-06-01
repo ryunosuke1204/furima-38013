a75cdea07f2312a8150a8f76792d79b2c2556f48
ryunosuke1204 <boxing.ryu1204@gmail.com>
2022-06-01 20:31:25 +0900

    商品購入の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,36 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+  before_action :non_purchased_item, only: [:index, :create]
+
+  def index
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price, 
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
+      @order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code, :sipping_area_id, :municipalities, :address, :building,:phone_number,:purchase_management).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
+  end
+
+  def non_purchased_item
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase_management.present?
+  end
+
+end
---
+++ app/models/order.rb
@@ -0,0 +1,19 @@
+class Order
+  include ActiveModel::Model
+  attr_accessor :post_code, :sipping_area_id, :municipalities, :address, :building, :phone_number, :user_id, :item_id,:token
+  
+  validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "例）123-4567"}
+  validates :sipping_area_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :municipalities,presence: true
+  validates :address,presence: true
+  validates :phone_number, format: {with: /\d{10,11}/}, length: {maximum: 11}
+  validates :user_id,presence: true
+  validates :item_id,presence: true
+  validates :token, presence: true
+
+  def save
+    order = PurchaseManagement.create(user_id:user_id, item_id:item_id)
+    ShippingAddress.create(post_code:post_code, sipping_area_id:sipping_area_id, municipalities:municipalities, address:address,building:building,phone_number:phone_number)
+  end  
+  
+end
